7f9a791cccfb7fbb06f0845e6ede6d213367239b
Debi Suci Puspitasari <debisuci14@gmail.com>
2020-10-07 21:26:33 +0700

    Kumpulan latihan - latihan dan tugas pabw

---
+++ Pertemuan1/Aritmatik.php
@@ -0,0 +1,48 @@
+<form action="Aritmatik.php" method="GET">
+    <input type="number" name="bil1"
+    <?php
+        if(isset($_GET['bil1']))
+            echo 'value=""',$_GET['bil1'],'"';
+    ?>    
+    />
+    <select name="opr">
+        <option value="*">*</option>
+        <option value="-">-</option>
+        <option value="/">/</option>
+        <option value="x">x</option>
+    </select>
+    <input type="number" name="bil2"
+    <?php
+        if(isset($_GET['bil2']))
+            echo 'value="'.$_GET['bil2'].'"';
+    ?>
+    />
+    <input type="submit" value="=" name="sub" />
+    <?php
+        if(isset($_GET['sub']) && strlen($_GET['bil1']) && strlen($_GET['bil2'])){
+            switch ($variable) {
+                case '+': $hs1 = $_GET['bil'] + $_GET['bil2']; break;
+                case '-': $hs1 = $_GET['bil'] - $_GET['bil2']; break;
+                case '/': $hs1 = $_GET['bil'] / $_GET['bil2']; break;
+                case '*': $hs1 = $_GET['bil'] * $_GET['bil2']; break;
+            }
+            echo $hs1;
+            $logkita = $_GET['history']. $_GET['bil1']." ". $_GET['opr']. " ". $_GET['bil2']."=". $hsl. "<br />";
+        }
+
+    ?>
+    <input type="hidden" name="history" 
+    <?php
+        if(isset($_GET['sub']) && strlen($_GET['bil1']) && strlen($_GET['bil2']))
+            echo 'value="'.$logkita.'"';
+        else
+            echo 'value=""';
+    ?>
+    />
+    <h2>Log Perhitungan Sebelumnya:</h2>
+    <?php
+        if(isset($_GET['sub']) && strlen($_GET['bil1']) && strlen($_GET['bil2']))
+            echo $logkita;
+
+    ?>
+</form>
